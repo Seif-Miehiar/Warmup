1e34e3d03b19f7f5ae8434573dc7966a3a7332c6
Seif-Miehiar <Saif.muhiar97@gmail.com>
2019-03-27 09:30:19 +0200

    Seif Miehiar

--- lastWarmup.js
+++ lastWarmup.js
@@ -8,3 +8,22 @@ twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
 */
 
 
+function twoSum(array , target){
+
+var newArray = [];
+
+	for ( var i = 0; i < array.length; i++){
+
+		for ( var x = 0; x < array.length; x++){
+
+			if( array[i] + array[x] === target){
+				newArray.push(i,x)
+
+			}
+		}return newArray;
+	}
+
+}
+
+//for loop for iterating over the array
+//if condition that if array[0] + any other index should qual the target after the sum 
